0f01db7376115e0cfd778a109a3be3944e832d7a
Vladimir Vasilenko <9319669@gmail.com>
2023-05-25 22:42:44 +0300

    Main.java finished

---
+++ src/BonusMilesService.java
@@ -0,0 +1,6 @@
+public class BonusMilesService {
+    public int calculate(int ticketCost){
+        int miles = ticketCost / 20;
+        return miles;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        BonusMilesService service = new BonusMilesService();
+        int ticketCost = 27845;
+        int miles = service.calculate(ticketCost);
+        System.out.println("При стоимости билета: " + ticketCost + " рублей(я), " + "Колличество начисленных Вам бонусных миль равно: "+ miles);
 
         }
     }
